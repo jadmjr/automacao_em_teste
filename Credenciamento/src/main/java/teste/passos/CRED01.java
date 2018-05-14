cd000bdd6b3b569dea7f04e4df5831f9c5864ba2
Jadson <jadson.monteiro@cedrotech.com>
2018-05-14 15:22:34 -0300

    Teste

--- Credenciamento/src/main/java/teste/passos/CRED01.java
+++ Credenciamento/src/main/java/teste/passos/CRED01.java
@@ -2,10 +2,9 @@ package teste.passos;
 
 import static org.junit.Assert.fail;
 
-import java.util.concurrent.TimeUnit;
-
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -25,9 +24,6 @@ public class CRED01 extends Config {
 	@Dado("^o browser \"(.*?)\" inserir a url \"([^\"]*)\" da aplicação$")
 	public void acessarAplicacao(String browser, String url) throws Exception {
 		try {
-			// JMJR
-			System.out.println("Acessar aplicacao");
-
 			if (browser.equals("chrome")) {
 
 				// Configurando opções do navegador
@@ -38,78 +34,69 @@ public class CRED01 extends Config {
 				// Inicializando Nav.
 				driver = new ChromeDriver(options);
 				driver.get(url);
-				driver.manage().timeouts().setScriptTimeout(90, TimeUnit.SECONDS);
-				// driver.findElement(By.name("q")).sendKeys("Jadson Uberaba");
-				// System.out.println("jadson uberaba");
+				//
+
 			}
 		} catch (Exception e) {
 			System.out.println("Erro - " + e.getMessage());
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Entao("^confirmada a abertura da pagina atraves do titulo \"([^\"]*)\"$")
 	public void acesso(String texto) throws Exception {
 		try {
-			// JMJR
-			System.out.println("acesso");
-
 			// Testando Acesso a Página pelo título
 			Assert.assertEquals(driver.getTitle(), texto);
 
 		} catch (Exception e) {
 			System.out.println("Erro - " + e.getMessage());
-			fail();driver.close();
+			fail();
+			driver.close();
 		} catch (AssertionError e) {
 			System.out.println("Erro - " + e.getMessage());
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
-
-	// Inserir um cnpj válido e não cadastrado no sistema e escolher a opção de um
-	// novo cadastro
 
 	@Dado("^preencher cnpj\"([^\"]*)\"$")
 	public void InserirCPNJ(String cnpj) {
 		try {
-			// JMJR
-			System.out.println("inserir CNPJ");
-
 			// Esperando carregar input
-			WebElement campoCNPJ = (new WebDriverWait(driver, 10))
-					.until(ExpectedConditions.presenceOfElementLocated(By.id("inlineFormInputGroup")));
+			WebDriverWait wait = new WebDriverWait(driver, 10);
+			WebElement campoCNPJ = wait.until(ExpectedConditions.presenceOfElementLocated(By.id(idCampoCNPJ)));
 
 			campoCNPJ.sendKeys(cnpj);
 
 		} catch (Exception e) {
 			System.out.println("Erro - " + e.getMessage());
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Dado("^clicar no botão lupa$")
 	public void clicarLupa() {
 		try {
-			// JMJR
-			System.out.println("Clicar na lupa");
 			// Clique lupa
-			driver.findElement(By.id(camBtnLupa)).click();
+			driver.findElement(By.id(idBtnLupa)).click();
 
 		} catch (Exception e) {
 			System.out.println("Erro - " + e.getMessage());
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@E("confirmar a inserção de um novo cadastro$")
-	public void confirmarNovoCadastro() throws Exception{
+	public void confirmarNovoCadastro() throws Exception {
 		try {
-			WebElement btnConfirmar = (new WebDriverWait(driver,2)).until(
-			ExpectedConditions.visibilityOfElementLocated(By.xpath("(//button[@id='btAbreProposta'])[2]")));
-			btnConfirmar.click();
-
+			WebDriverWait wait = new WebDriverWait(driver, 10);
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(idBtnConfirmaCad))).click();
 		} catch (Exception e) {
-			System.out.println("Erro - será aqui" + e.getMessage());
+			System.out.println("Erro - " + e.getMessage());
 			driver.close();
 		}
 	}
@@ -118,281 +105,448 @@ public class CRED01 extends Config {
 	public void AbrirTelaNovoCadastro() {
 		try {
 			// JMJR
-			System.out.println("abrir tela novo cadastro");
-			// JMJR ESPERA IMPLICIDA
-			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			Thread.sleep(3000);
+			// FORÇANDO REMOCAO DO FADE
+
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			WebElement fade = driver.findElement(By.xpath(xpathFade));
+
+			js.executeScript(
+					"document.getElementsByClassName('modal-backdrop fade show').setAttribute('class', 'modal-backdrop fade hide')",
+					fade);
+			System.out.println("removeu fade");
 
 			// ESPERANDO
-			WebElement campoRazaoSocial = (new WebDriverWait(driver, 10))
-					.until(ExpectedConditions.presenceOfElementLocated(By.name("razaoSocial")));
+			WebDriverWait wait = new WebDriverWait(driver, 10);
+			WebElement campoRazaoSocial = wait
+					.until(ExpectedConditions.presenceOfElementLocated(By.name(nomeCampoRazaoSocial)));
 
 			Assert.assertNotNull(campoRazaoSocial);
-			System.out.println("afirmou o não nulo");
+
 		} catch (Exception e) {
 			System.out.println("Erro - " + e.getMessage());
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Dado("^a razão Social:\"([^\"]*)\"$")
 	public void PrecherRazaoSocial(String razaoSocial) {
 		try {
-			// JMJR
-			System.out.println("PrecherRazaoSocial");
 
-			// ESPERANDO
-			WebElement campoRazaoSocial = (new WebDriverWait(driver, 10))
-					.until(ExpectedConditions.presenceOfElementLocated(By.name("razaoSocial")));
+			driver.findElement(By.name(nomeCampoRazaoSocial)).sendKeys(razaoSocial);
 
-			campoRazaoSocial.sendKeys(razaoSocial);
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Dado("^o Nome fantasia:\"([^\"]*)\"$")
 	public void PrecherNomeFantasia(String nomeFantasia) {
 		try {
-			// JMJR
-			System.out.println("PrecherNomeFantasia");
 
-			// ESPERANDO
-			WebElement campoNomeFantasia = (new WebDriverWait(driver, 10))
-					.until(ExpectedConditions.presenceOfElementLocated(By.name("nomeFantasia")));
+			driver.findElement(By.name(nomeCampoNomeFantasia)).sendKeys(nomeFantasia);
 
-			campoNomeFantasia.sendKeys(nomeFantasia);
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Dado("^a Data de fundação:\"([^\"]*)\"$")
 	public void PrecherDtFundacao(String dtfundacao) {
 		try {
-			// JMJR
-			System.out.println("PrecherDtFundacao");
 
-			// ESPERANDO
-			WebElement campoDtFundacao = (new WebDriverWait(driver, 10))
-					.until(ExpectedConditions.presenceOfElementLocated(By.name("dataFundacao")));
+			driver.findElement(By.name(nomeCampoDataFundacao)).sendKeys(dtfundacao);
 
-			campoDtFundacao.sendKeys(dtfundacao);
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Dado("^preecher o Email:\"([^\"]*)\"$")
 	public void PrecherEmail(String email) {
 		try {
-			// JMJR
-			System.out.println("PrecherEmail");
 
-			// ESPERANDO
-			WebElement campoEmail = (new WebDriverWait(driver, 10))
-					.until(ExpectedConditions.presenceOfElementLocated(By.name("emaildadoscadastrais")));
+			driver.findElement(By.name(nomeCampoEmailDadosCadastrais)).sendKeys(email);
 
-			campoEmail.sendKeys(email);
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Dado("^o Telefone:\"([^\"]*)\"$")
-	public void PrecherTelefone(String telefone) {
+	public void precherTelefoneEmpresa(String telefone) {
 		try {
-			// JMJR
-			System.out.println("PrecherTelefone");
 
-			// ESPERANDO
-			WebElement campoTelefone = (new WebDriverWait(driver, 10))
-					.until(ExpectedConditions.presenceOfElementLocated(By.name("telefone")));
+			driver.findElement(By.id(idCampoTelefone)).sendKeys(telefone);
 
-			campoTelefone.sendKeys(telefone);
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Entao("^Verificar se a tela exibe a box com os dados da empresa salvos$")
 	public void ValidarBoxEmpresa() {
 		try {
-			// JMJR
-			System.out.println("ValidarBoxEmpresa");
 
-			// ESPERANDO
-			WebElement btnSalvar = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(
-					By.xpath("//*[@id=\"CorpoProposta\"]/div[1]/div[2]/div[1]/div[2]/div/div[2]/div[2]/button")));
+			driver.findElement(By.xpath(xpathBoxEmpresa)).click();
 
-			btnSalvar.click();
-
-			WebElement btnEditar = driver.findElement(By.xpath("//*[@id=\"formInfoCadastrais\"]/div[2]/div/button"));
+			WebElement btnEditar = driver.findElement(By.xpath(xpathBtnEditarEmpresa));
 			Assert.assertNotNull(btnEditar);
-			// JMJR
-			System.out.println("CLICOU EM SALVAR");
+
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Dado("^o cep\"([^\"]*)\"$")
 	public void preencherCep(String cep) {
 		try {
-			// JMJR
-			System.out.println("preencherCep");
-
-			// ESPERANDO
-			driver.findElement(By.id("cep")).sendKeys(cep);
+			driver.findElement(By.id(idCampoCEP)).sendKeys(cep);
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Dado("^o numero \"([^\"]*)\"$")
 	public void preencherNumero(String numero) throws Throwable {
 		try {
-			// JMJR
-			System.out.println("preencherNumero");
-			// JMJR ESPERA IMPLICIDA PARA ESPERAR O WEBSERVICE
-			driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
-			// ESPERANDO
-			WebElement campoEndereco = driver.findElement(By.id("endereco"));
-			campoEndereco.sendKeys(campoEndereco.getText() + ", " + numero);
+			// todo
+			WebElement campoEndereco = driver.findElement(By.id(idCampoEndereco));
+			// JMJR - ESPERANDO WS
+			campoEndereco.click();
+			Thread.sleep(2000);
+			campoEndereco.sendKeys(campoEndereco.getText() + " , " + numero);
 
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Dado("^o complemento \"([^\"]*)\"$")
 	public void preencherComplemento(String complemento) {
 		try {
-			// JMJR
-			System.out.println("preencherComplemento");
 
-			// ESPERANDO
-			driver.findElement(By.id("complemento")).sendKeys(complemento);
+			driver.findElement(By.id(idCampoComplemento)).sendKeys(complemento);
 
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Entao("^clicar em salvar e confirmar box endereco$")
 	public void salvarEndereco() {
 		try {
-			// JMJR
-			System.out.println("salvarEndereco");
-
 			// ESPERANDO
-			driver.findElement(
-					By.xpath("//*[@id=\"CorpoProposta\"]/div[1]/div[2]/div[1]/div[4]/div/div[2]/div[2]/button"))
-					.click();
-			WebElement btnEditar = driver.findElement(
-					By.xpath("//*[@id=\"CorpoProposta\"]/div[1]/div[2]/div[1]/div[3]/div/div[2]/div/button"));
+			driver.findElement(By.xpath(xpathSalvarEndereco)).click();
+			WebElement btnEditar = driver.findElement(By.xpath(xpathEditarEndereco));
 			Assert.assertNotNull(btnEditar);
 
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Dado("^o nome do socio \"([^\"]*)\"$")
 	public void preencherSocio(String nomeSocio) {
 		try {
-			// JMJR
-			System.out.println("preencherSocio");
-
-			driver.findElement(By.name("nome")).sendKeys(nomeSocio);
+			driver.findElement(By.name(nomeCampoNomeSocio)).sendKeys(nomeSocio);
 
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Dado("^e o cpf \"([^\"]*)\"$")
 	public void preecherCPF(String cpf) {
 		try {
-			// JMJR
-			System.out.println("preecherCPF");
-
-			driver.findElement(By.name("cpf")).sendKeys(cpf);
+			driver.findElement(By.name(nomeCampoCpf)).sendKeys(cpf);
 
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Dado("^a porcentagem \"([^\"]*)\"$")
 	public void preencherPorcentagem(String porcentagem) {
 		try {
-			// JMJR
-			System.out.println("preencherPorcentagem");
-
-			driver.findElement(By.name("participacao")).sendKeys(porcentagem);
+			driver.findElement(By.name(nomeCampoParticipacao)).sendKeys(porcentagem);
 
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Dado("^a data de nascimento\"([^\"]*)\"$")
 	public void preencherDataNascimento(String dtNascimento) {
 		try {
-			// JMJR
-			System.out.println("preencherDataNascimento");
-
-			driver.findElement(By.name("dataNascimento")).sendKeys(dtNascimento);
+			driver.findElement(By.name(nomeCampoDataNascimento)).sendKeys(dtNascimento);
 
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Dado("^o telefone \"([^\"]*)\"$")
-	public void preecherTelefone(String telefone) {
+	public void preecherTelefoneSocio(String telefone) {
 		try {
-			// JMJR
-			System.out.println("preecherTelefone");
-			System.out.println("telefoneSocio: " + telefone);
-			driver.findElement(By.xpath(
-					"//*[@id=\"CorpoProposta\"]/div[1]/div[2]/div[2]/div[3]/div/div/div[1]/form/div[2]/div[6]/div/input"))
-					.sendKeys(telefone);
+			driver.findElement(By.xpath(xpathCampoTelefoneSocio)).sendKeys(telefone);
 
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Dado("^o email\"([^\"]*)\"$")
 	public void preencherEmail(String email) {
 		try {
-			// JMJR
-			System.out.println("preencherEmail");
-
-			driver.findElement(By.name("email")).sendKeys(email);
+			driver.findElement(By.name(nomeCampoEmail)).sendKeys(email);
 
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
 		}
 	}
 
 	@Entao("^clicar em salvar e confirmar box socio$")
 	public void salvarBoxSocio() {
 		try {
-			// JMJR
-			System.out.println("salvarBoxSocio");
-
 			// ESPERANDO
-			driver.findElement(
-					By.xpath("//*[@id=\"CorpoProposta\"]/div[1]/div[2]/div[2]/div[3]/div/div/div[2]/div[3]/button"))
-					.click();
-			WebElement btnEditar = driver.findElement(
-					By.xpath("//*[@id=\"CorpoProposta\"]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div[1]/button"));
+			driver.findElement(By.xpath(xpathBtnSalvaSocio)).click();
+			WebElement btnEditar = driver.findElement(By.xpath(xpathEditaSocio));
 			Assert.assertNotNull(btnEditar);
 
 		} catch (Exception e) {
-			fail();driver.close();
+			fail();
+			driver.close();
+		}
+	}
+
+	@Dado("^o cnae \"([^\"]*)\"$")
+	public void preencherCNAE(String cnae) {
+		try {
+			driver.findElement(By.id(idCampoCNAE)).sendKeys(cnae);
+
+		} catch (Exception e) {
+			fail();
+			driver.close();
+		}
+	}
+
+	@Dado("^o faturamento mensal \"([^\"]*)\"$")
+	public void preecherFaturamentoMensal(String fatMensal) {
+		try {
+			driver.findElement(By.id(idCampoFaturamento)).sendKeys(fatMensal);
+
+		} catch (Exception e) {
+			fail();
+			driver.close();
+		}
+	}
+
+	@Dado("^a previsao de venda \"([^\"]*)\"$")
+	public void preencherPrevisaoVenda(String ItemPrevVenda) throws Throwable {
+		try {
+			WebElement select = driver.findElement(By.id(idFormPrevisaoVenda));
+			new org.openqa.selenium.support.ui.Select(select).selectByIndex(Integer.parseInt(ItemPrevVenda));
+
+		} catch (Exception e) {
+			fail();
+			driver.close();
+		}
+	}
+
+	@Entao("^confirmar habilitacao menu de planos$")
+	public void confirmarCampoPlanos() {
+		try {
+			// TODO
+
+		} catch (Exception e) {
+			fail();
+			driver.close();
+		}
+	}
+
+	@Dado("^adicionar solução POS com fio \"([^\"]*)\"$")
+	public void addPosComFio(String qtdComFio) {
+		try {
+			driver.findElement(By.xpath(xpathQtdPOSComFio)).sendKeys(qtdComFio);
+			driver.findElement(By.xpath(xpathAddPOSComFio)).click();
+			Thread.sleep(2000);
+
+		} catch (Exception e) {
+			fail();
+			driver.close();
+		}
+	}
+
+	@Entao("^confirmar inclusao POS com fio no carrinho \"([^\"]*)\"$")
+	public void confirmar_inclusao_POS_com_fio_no_carrinho(String qtdPOS) throws Throwable {
+
+		WebElement campoQtdPOS = driver.findElement((By.xpath(xpathCarrinhoPOScomFio)));
+
+		Assert.assertEquals(campoQtdPOS.getText(), qtdPOS);
+
+		throw new PendingException();
+	}
+
+	@Dado("^adicionar solução POS sem fio	\"([^\"]*)\"$")
+	public void addPosSemFio(String qtdSemFio) {
+		try {
+			driver.findElement(By.xpath(xpathQtdPOSSemFio)).sendKeys(qtdSemFio);
+			driver.findElement(By.xpath(xpathAddPOSemFio)).click();
+			Thread.sleep(2000);
+
+		} catch (Exception e) {
+			fail();
+			driver.close();
+		}
+	}
+
+	@Entao("^confirmar inclusao POS sem fio no carrinho \"([^\"]*)\"$")
+	public void confirmar_inclusao_POS_sem_fio_no_carrinho(String qtdPOS) throws Throwable {
+
+		WebElement campoQtdPOS = driver.findElement((By.xpath(xpathCarrinhoPOSsemFio)));
+
+		Assert.assertEquals(campoQtdPOS.getText(), qtdPOS);
+
+		throw new PendingException();
+	}
+
+	@Dado("^adicionar solução POS mobile	\"([^\"]*)\"$")
+	public void addPosMobile(String qtdPosMobile) {
+		try {
+			driver.findElement(By.xpath(xpathQtdPOSMobile)).sendKeys(qtdPosMobile);
+			driver.findElement(By.xpath(xpathAddPOSMobile)).click();
+			Thread.sleep(2000);
+
+		} catch (Exception e) {
+			fail();
+			driver.close();
+		}
+	}
+
+	@Entao("^confirmar inclusao POS mobile no carrinho \"([^\"]*)\"$")
+	public void confirmar_inclusao_POS_mobile_no_carrinho(String qtdPOS) throws Throwable {
+
+		WebElement campoQtdPOS = driver.findElement((By.xpath(xpathCarrinhoPOSMobile)));
+
+		Assert.assertEquals(campoQtdPOS.getText(), qtdPOS);
+	}
+
+	@Dado("^a bandeira \"([^\"]*)\"$")
+	public void preencherBandeira(String itemBandeira) {
+		try {
+			WebElement select = driver.findElement(By.id(xpathSelectBandeira));
+			new org.openqa.selenium.support.ui.Select(select).selectByIndex(Integer.parseInt(itemBandeira));
+			// JMJR
+			System.out.println("escolheu item");
+
+		} catch (Exception e) {
+			fail();
+			driver.close();
+		}
+	}
+
+	@Dado("^o banco \"([^\"]*)\"$")
+	public void preencherBanco(String banco) {
+		try {
+			driver.findElement(By.id(idCampoBanco)).sendKeys(banco);
+
+		} catch (Exception e) {
+			fail();
+			driver.close();
+		}
+	}
+
+	@Dado("^a agencia \"([^\"]*)\"$")
+	public void preencherAgencia(String agencia) {
+		try {
+			driver.findElement(By.id(idCampoAgencia)).sendKeys(agencia);
+
+		} catch (Exception e) {
+			fail();
+			driver.close();
+		}
+	}
+
+	@Dado("^a conta \"([^\"]*)\"$")
+	public void preencherConta(String conta) {
+		try {
+			driver.findElement(By.name(nomeCampoConta)).sendKeys(conta);
+
+		} catch (Exception e) {
+			fail();
+			driver.close();
+		}
+	}
+
+	@Entao("^clicar no botao para adicionar$")
+	public void clicalBotaoAdcionar() {
+		try {
+			driver.findElement(By.xpath(xpathAddDomicilioBanc));
+
+		} catch (Exception e) {
+			fail();
+			driver.close();
+		}
+	}
+
+	@Dado("^email do agente credenciador \"([^\"]*)\"$")
+	public void preencherEmailCredenciador(String emailCred) {
+		try {
+			driver.findElement(By.xpath(xpathCampoEmailCred)).sendKeys(emailCred);
+
+		} catch (Exception e) {
+			fail();
+			driver.close();
+		}
+	}
+
+	@Dado("^nome da pessoa que recebera o esquipamento \"([^\"]*)\"$")
+	public void preencherReceptorEquipamento(String receptor) {
+		try {
+			driver.findElement(By.xpath(xpathReceptor)).sendKeys(receptor);
+
+		} catch (Exception e) {
+			fail();
+			driver.close();
+		}
+	}
+
+	@Dado("^as observacoes \"([^\"]*)\"$")
+	public void preencherObservacoes(String observacoes) {
+		try {
+			driver.findElement(By.xpath(xpathObervacoes)).sendKeys(observacoes);
+
+		} catch (Exception e) {
+			fail();
+			driver.close();
+		}
+	}
+
+	@Entao("^enviar proposta$")
+	public void enviarProposta() {
+		try {
+			driver.findElement(By.xpath(xpathBtnEnviarProposta)).click();
+		} catch (Exception e) {
+			fail();
+			driver.close();
 		}
 	}
 
